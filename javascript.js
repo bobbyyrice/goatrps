e181e5bb6599b05b33d4a8623b7c07a64f94e6b8
bobbyyrice <kirboepik@gmail.com>
2024-08-02 16:38:03 -1000

    asd

--- javascript.js
+++ javascript.js
@@ -3,14 +3,15 @@ const button2 = document.getElementById("button2")
 const button3 = document.getElementById("button3")
 const button4 = document.getElementById("button4")
 
-const words = document.getElementById("textinthebox")
+const textBox = document.getElementById("bigboxfortext")
 
-function displayDate() {    
-    let date = " The date is : " + Date();
-    return date;
+function addElement(theText) {
+    const addText = document.createElement("p");
+    addText.innerHTML = theText;
+    document.getElementById("bigboxfortext").appendChild(addText);
 }
 
-button1.addEventListener("click", function(){words.textContent += displayDate();})
-button2.addEventListener("click", function(){words.textContent += " hello gang"})
-button3.addEventListener("click", function(){words.textContent += " hello world"})
-button4.addEventListener("click", function(){words.innerHTML = "hello world"})
+button1.addEventListener("click", function(){})
+button2.addEventListener("click", function(){})
+button3.addEventListener("click", function(){})
+button4.addEventListener("click", function(){})
